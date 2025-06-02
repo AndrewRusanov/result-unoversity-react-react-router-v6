64ddf20746a5a0afc2dd334766ee5f2cb19ce1a8
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-06-02 19:14:25 +0300

    feat: add Not Found page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from '@/widgets'
 import { Home } from '@pages/Home'
+import { NotFound } from '@pages/NotFound'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path='/episodes/:id' element={<div>Эпизод с ID</div>} />
           <Route path='/locations' element={<div>Локации</div>} />
           <Route path='/locations/:id' element={<div>Локация с ID</div>} />
-          <Route path='*' element={<div>404</div>} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </main>
     </div>
